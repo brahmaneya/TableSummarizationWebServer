838d416304f7e9daf4bf4673641089ae2c5112df
brahmaneya <brahmaneya@gmail.com>
2015-08-22 22:34:14 -0700

    Corrected weird hashing bug(?) by replacing valueMap by values in overriden equals and hashCode for Rule

--- TT/src/servlets/MarketingSDD.java
+++ TT/src/servlets/MarketingSDD.java
@@ -39,7 +39,8 @@ import dataextraction.TableInfo;
 @WebServlet("/MarketingSDD")
 public class MarketingSDD extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    String message;   
+    String message;  
+    TableInfo table;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -113,118 +114,8 @@ public class MarketingSDD extends HttpServlet {
 			columns.add(i);
 		}
 		
-		TableInfo table = fullTable.getSubTable(columns);
-		/*Integer ruleNums = Integer.parseInt(args[0]); 
-		Integer maxRuleScore = Integer.parseInt(args[1]); // program input
-		final Scorer scorer; // program input
-		switch(args[2]) {
-			case "Size" :
-				scorer = new Rule.sizeScorer(); 
-				break;
-			case "Bits" :
-				scorer = new Rule.sizeBitsScorer();
-				break;
-			case "Square" :
-				scorer = new Rule.sizeSquareScorer();
-				break;
-			case "Min2" : 
-				scorer = new Rule.sizeMinusKScorer(1);
-				break;
-			default :
-				scorer = new Rule.sizeScorer();
-				break;
-		}
+		table = fullTable.getSubTable(columns);
 		
-		String ruleString = args[3];
-		ruleString = ruleString.substring(1, ruleString.length() - 1);
-		String[] vals = ruleString.split(",");
-		List<Integer> ruleVals = new ArrayList<Integer>();
-		for (int col = 0; col < table.dictionary.size(); col++) {
-			final String valString = vals[col];
-			ruleVals.add(-1);
-			for (int val = 0; val < table.dictionary.get(col).size(); val++) {
-				if (table.getName(col, val).equals(valString)) {
-					ruleVals.set(col, val);
-					break;
-				}
-			}
-		}
-		Rule rule = new Rule(ruleVals);
-		final List<Integer> defaultCols = new ArrayList<Integer>();
-		final List<Integer> ignoreCols = new ArrayList<Integer>();
-		final List<Integer> forceCols = new ArrayList<Integer>();
-		String coloptsString = args[4];
-		String[] colopts = coloptsString.split(",");
-		for (String colopt : colopts) {
-			int colonLocation = colopt.lastIndexOf(":");
-			final String colName = colopt.substring(0, colonLocation);
-			final String status = colopt.substring(colonLocation + 1); 
-			int colNo = -1;
-			for (colNo = 0; colNo < table.dictionary.size(); colNo++) {
-				if(table.names.get(colNo).get("column").equals(colName)) {
-					break;
-				}
-			}
-			switch (status) {
-				case "Default" :
-					defaultCols.add(colNo);
-					break;
-				case "Ignore" :
-					ignoreCols.add(colNo);
-					break;
-				case "Force" :
-					forceCols.add(colNo);
-					break;
-			}
-		}
-		Scorer modifiedScorer = new Scorer () {
-			@Override
-			public void setScore(TableInfo table, Rule rule) {
-				for (Integer col : forceCols) {
-					if (rule.get(col) == -1) {
-						rule.score = 0;
-						return;
-					}
-				}
-				Rule tempRule = rule.deepValuesCopy();
-				for (Integer col : ignoreCols) {
-					tempRule.addVal(col, -1);
-				}
-				scorer.setScore(table, tempRule);
-				rule.score = tempRule.score;
-			}
-		};
-		int minSampleSize = Integer.MAX_VALUE;
-		int capacity = Integer.MAX_VALUE;
-		SampleHandler sampleHandler = new SampleHandler(table, capacity, minSampleSize);
-		Set<Rule> solutionSet = null;
-		try{
-			solutionSet = NonStarCountSolvers.getSolution (table, rule, ruleNums, maxRuleScore, modifiedScorer, -1, sampleHandler);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		String solutionString = "[";
-		boolean start = true;
-		for (Rule solRule : solutionSet) {		
-			if(!start) {
-				solutionString = solutionString + ",";
-			} else {
-				start = false;				
-			}
-			solutionString = solutionString + "{\"vals\":[";
-			for (Integer col = 0; col < table.names.size(); col++) {
-				if (col > 0) {
-					solutionString = solutionString + ",";
-				}
-				solutionString = solutionString + "\"" + table.getName(col, solRule.get(col)) + "\"";
-			}
-			solutionString = solutionString + "," + solRule.count;
-			solutionString = solutionString + "," + solRule.score;
-			solutionString = solutionString + "]}";
-		}
-		solutionString = solutionString + "]";
-		out.println(solutionString);
-    	*/
     }
     
     public void doOptions(HttpServletRequest req, HttpServletResponse resp)
@@ -266,12 +157,12 @@ public class MarketingSDD extends HttpServlet {
 		line = br.readLine();
 		line = line.substring(1, line.length() - 1);
 		line = line + ",";
-		int k = Integer.parseInt(line.substring(4 + line.indexOf("\"k\":"), line.indexOf(",", line.indexOf("\"k\":"))));
-		int mw = Integer.parseInt(line.substring(5 + line.indexOf("\"mw\":"), line.indexOf(",", line.indexOf("\"mw\":"))));
-		int rowNo = Integer.parseInt(line.substring(8 + line.indexOf("\"rowNo\":"), line.indexOf(",", line.indexOf("\"rowNo\":"))));
-		String W = line.substring(5 + line.indexOf("\"W\":\""), line.indexOf("\",", line.indexOf("\"W\":\"")));
+		final int ruleNums = Integer.parseInt(line.substring(4 + line.indexOf("\"k\":"), line.indexOf(",", line.indexOf("\"k\":"))));
+		final int maxRuleScore = Integer.parseInt(line.substring(5 + line.indexOf("\"mw\":"), line.indexOf(",", line.indexOf("\"mw\":"))));
+		final int rowNo = Integer.parseInt(line.substring(8 + line.indexOf("\"rowNo\":"), line.indexOf(",", line.indexOf("\"rowNo\":"))));
+		final String W = line.substring(5 + line.indexOf("\"W\":\""), line.indexOf("\",", line.indexOf("\"W\":\"")));
 		String coloptString = line.substring(10 + line.indexOf("\"colopt\":{"), line.indexOf("}", line.indexOf("\"colopt\":{")));
-		coloptString.replaceAll("\"", "");
+		coloptString = coloptString.replaceAll("\"", "");
 		
 		String rulesString = line.substring(9 + line.indexOf("\"rules\":["), 1 + line.indexOf("}]", line.indexOf("\"rules\":[")));
 		rulesString = rulesString.substring(1, rulesString.length() - 1);
@@ -285,6 +176,120 @@ public class MarketingSDD extends HttpServlet {
 			depthsList.add(Integer.parseInt(s.substring(8 + s.indexOf("\"depth\":"), s.indexOf(",", s.indexOf("\"depth\":")))));
 			expandedsList.add(s.substring(11 + s.indexOf("\"expanded\":"), 12 + s.indexOf("\"expanded\":")).equals("1"));
 		}
+		if (expandedsList.get(rowNo)) {
+			final int depth = depthsList.get(rowNo);
+			while (depthsList.get(rowNo + 1) > depth) {
+				valsList.remove(rowNo + 1);
+				depthsList.remove(rowNo + 1);
+				expandedsList.remove(rowNo + 1);
+			}
+			//pw.print(rulesJSONString(valsList, depthsList, expandedsList));
+			out.print(rulesJSONString(valsList, depthsList, expandedsList));
+		} else {
+			final Scorer scorer; // program input
+			switch(W) {
+				case "Size" :
+					scorer = new Rule.sizeScorer(); 
+					break;
+				case "Bits" :
+					scorer = new Rule.sizeBitsScorer();
+					break;
+				case "Square" :
+					scorer = new Rule.sizeSquareScorer();
+					break;
+				case "Min2" : 
+					scorer = new Rule.sizeMinusKScorer(1);
+					break;
+				default :
+					scorer = new Rule.sizeScorer();
+					break;
+			}
+			
+			String ruleString = valsList.get(rowNo);
+			ruleString = ruleString.substring(1, ruleString.length() - 1);
+			String[] vals = ruleString.split("\",\"");
+			List<Integer> ruleVals = new ArrayList<Integer>();
+			for (int col = 0; col < table.dictionary.size(); col++) {
+				final String valString = vals[col];
+				ruleVals.add(-1);
+				for (int val = 0; val < table.dictionary.get(col).size(); val++) {
+					if (table.getName(col, val).equals(valString)) {
+						ruleVals.set(col, val);
+						break;
+					}
+				}
+			}
+			////// check here is we find valid rule. due to " etc.
+			
+			
+			Rule rule = new Rule(ruleVals);
+			final List<Integer> defaultCols = new ArrayList<Integer>();
+			final List<Integer> ignoreCols = new ArrayList<Integer>();
+			final List<Integer> forceCols = new ArrayList<Integer>();
+			String[] colopts = coloptString.split(",");
+			for (String colopt : colopts) {
+				int colonLocation = colopt.lastIndexOf(":");
+				final String colName = colopt.substring(0, colonLocation);
+				final String status = colopt.substring(colonLocation + 1); 
+				int colNo = -1;
+				for (colNo = 0; colNo < table.dictionary.size(); colNo++) {
+					if(table.names.get(colNo).get("column").equals(colName)) {
+						break;
+					}
+				}
+				switch (status) {
+					case "Default" :
+						defaultCols.add(colNo);
+						break;
+					case "Ignore" :
+						ignoreCols.add(colNo);
+						break;
+					case "Force" :
+						forceCols.add(colNo);
+						break;
+				}
+			}
+			Scorer modifiedScorer = new Scorer () {
+				@Override
+				public void setScore(TableInfo table, Rule rule) {
+					for (Integer col : forceCols) {
+						if (rule.get(col) == -1) {
+							rule.score = 0;
+							return;
+						}
+					}
+					Rule tempRule = rule.deepValuesCopy();
+					for (Integer col : ignoreCols) {
+						tempRule.addVal(col, -1);
+					}
+					scorer.setScore(table, tempRule);
+					rule.score = tempRule.score;
+				}
+			};
+			int minSampleSize = Integer.MAX_VALUE;
+			int capacity = Integer.MAX_VALUE;
+			SampleHandler sampleHandler = new SampleHandler(table, capacity, minSampleSize);
+			Set<Rule> solutionSet = null;
+			try{
+				solutionSet = NonStarCountSolvers.getSolution (table, rule, ruleNums, maxRuleScore, modifiedScorer, -1, sampleHandler);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			expandedsList.set(rowNo, true);
+			final int depth = depthsList.get(rowNo) + 1;
+			for (Rule solRule : solutionSet) {	
+				// check order here, maybe it should be reversed. 
+				final String solString = createValString(solRule);
+				valsList.add(rowNo + 1, solString);
+				expandedsList.add(rowNo + 1, false);
+				depthsList.add(rowNo + 1, depth);
+			}
+			//pw.print(rulesJSONString(valsList, depthsList, expandedsList));	    	
+			out.print(rulesJSONString(valsList, depthsList, expandedsList));	    	
+		}
+		
+		/*
 		out.println(k);
 		out.println(mw);
 		out.println(W);
@@ -293,12 +298,55 @@ public class MarketingSDD extends HttpServlet {
 		out.println(valsList.toString());
 		out.println(depthsList.toString());
 		out.println(expandedsList.toString());
-		// row and depthStr can be generated on the fly
-		//List<String> ruleStringsList = new ArrayList<String>();
 		out.println(rowNo);
 		out.println(rulesString);
 		out.println(line);
-		
+		out.println(rulesJSONString(valsList, depthsList, expandedsList));	
+		*/	
+	}
+	
+	public String createValString (Rule rule) {
+		String answer = "";
+		for (Integer col = 0; col < table.names.size(); col++) {
+			if (col > 0) {
+				answer = answer + ",";
+			}
+			answer = answer + "\"" + table.getName(col, rule.get(col)) + "\"";
+		}
+		answer = answer + "," + rule.count;
+		answer = answer + "," + rule.score;
+		return answer;
+	}
+	
+	public String rulesJSONString (List<String> valStringList, List<Integer> depths, List<Boolean> expandeds) {
+		String answer = "[";
+		int rowNo = 0;
+		for (String val : valStringList) {
+			if (rowNo > 0) {
+				answer = answer + ",";
+			}
+			answer = answer + "{";
+			String depthStr = "";
+			for (int i = 0; i < depths.get(rowNo); i++) {
+				depthStr = depthStr + ">";
+			}
+			answer = answer + "\'depthStr\':\'" + depthStr + "\'";
+			answer = answer + ",";
+			
+			answer = answer + "\'row\':" + rowNo;
+			answer = answer + ",";
+
+			answer = answer + "\'depth\':" + depths.get(rowNo);
+			answer = answer + ",";
+			
+			answer = answer + "\'expanded\':" + (expandeds.get(rowNo) ? 1 : 0);
+			answer = answer + ",";
+			
+			answer = answer + "\'vals\':[" + val + "]}";
+			rowNo++;
+		}
+		answer = answer + "]";
+		return answer;
 	}
 
 }
